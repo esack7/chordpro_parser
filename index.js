5d4657eab0890490acae9ad90a6a2c5fa3aaec48
esack7 <isaacheist@gmail.com>
2017-12-11 13:41:38 -0800

    parse and create are both working, some bugs

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const parseChordPro = require('./lib/parse_chordPro.js');
-// const createChordPro = require('./lib/create_chordPro.js');
+// const parseChordPro = require('./lib/parse_chordPro.js');
+const createChordPro = require('./lib/create_chordPro.js');
 const input = require('./lib/input.js');
-// createChordPro(input())
-// parseChordPro(input());
+const output = require('./lib/output.js');
+
+//Create a chopro file
 input().then(song => {
-  console.log(parseChordPro(song));
+  output(createChordPro(song));
 });
+
+//Parse a chopro file to txt
+// input().then(song => {
+//   output(parseChordPro(song));
+// });
