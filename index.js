724a52cd0b93521fd8c839f09b7e7f730fd1c5af
esack7 <isaacheist@gmail.com>
2017-12-11 13:45:00 -0800

    removed some corpse code and reconfig input/output/index

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-// const parseChordPro = require('./lib/parse_chordPro.js');
-const createChordPro = require('./lib/create_chordPro.js');
+const parseChordPro = require('./lib/parse_chordPro.js');
+// const createChordPro = require('./lib/create_chordPro.js');
 const input = require('./lib/input.js');
 const output = require('./lib/output.js');
 
-//Create a chopro file
+//Parse a chopro file to txt
 input().then(song => {
-  output(createChordPro(song));
+  output(parseChordPro(song));
 });
 
-//Parse a chopro file to txt
+//Create a chopro file
 // input().then(song => {
-//   output(parseChordPro(song));
+//   output(createChordPro(song));
 // });
